94093d828d5e52d4fd0487a65688ceaf23971a7b
jemprojects <jemprojectsgit@gmail.com>
2023-12-06 11:35:19 -0300

    dia 6

---
+++ challenge06/index.ts
@@ -0,0 +1,22 @@
+function maxDistance(movements): number{
+    // Code here
+    let derecha = 0
+    let izquierda = 0
+    let extras = 0
+    let result = 0
+     for (const move of movements) {
+      if (move =='<'){
+        ++derecha
+      } else if (move =='>'){
+        ++izquierda
+      }else{
+        ++extras
+      }
+    }
+    if (derecha > izquierda){
+      result = derecha - izquierda + extras
+    }else{
+       result =  izquierda + extras - derecha
+    }
+    return result
+  }
