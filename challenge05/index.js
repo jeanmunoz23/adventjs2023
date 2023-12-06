b7bbaa7518a0e612f5ab13a0898c7b3ffed8c229
jemprojects <jemprojectsgit@gmail.com>
2023-12-06 11:21:34 -0300

    quinto desafio

---
+++ challenge05/index.js
@@ -0,0 +1,23 @@
+function cyberReindeer(road, time) {
+  let moves = [road];
+  let sledIndex = 0;
+  let barrierSymbol = ".";
+
+  for (let currentTime = 1; currentTime < time; currentTime++) {
+    if (currentTime === 5) {
+      road = road.replace(/\|/g, "*");
+    }
+
+    const newRoad = road.replace(/S[\.\*]/, `${barrierSymbol}S`);
+    
+    if (newRoad !== road) {
+      sledIndex++;
+      barrierSymbol = road[sledIndex];
+    }
+
+    road = newRoad;
+    moves.push(road);
+  }
+
+  return moves;
+}
